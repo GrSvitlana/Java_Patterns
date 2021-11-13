163471a6dd38e4ade592cd78707e27b75e3281da
shonmikl <mikhailegorov676@gmail.com>
2021-11-13 23:08:16 +0300

    two patterns was added

--- src/brige/Main.java
+++ src/brige/Main.java
@@ -3,6 +3,8 @@ package brige;
 public class Main {
     public static void main(String[] args) {
         Vehicle vehicle = new Car(new Audi());
+        Vehicle vehicle1 = new Track(new Audi());
         vehicle.drive();
+        vehicle1.drive();
     }
 }
